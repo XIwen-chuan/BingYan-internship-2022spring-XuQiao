9a611d9d6c0a5aacccbed94a4e085d4dfb5d47f4
Xi_wenchuan <xq1033419145@163.com>
2022-03-11 20:49:04 +0800

    3-11，16：00-20：50

--- nodejs/ChildPageRefresh copy.js
+++ nodejs/ChildPageRefresh copy.js
@@ -8,12 +8,7 @@ var result;
 
 
 
-fs.readFile('./index.html', 'utf-8', function(err, data) {
-    if (err) { console.log(err) } else {
-        console.log(data);
-        result = data;
-    }
-})
+
 
 
 
@@ -28,22 +23,12 @@ fs.readFile('./index.html', 'utf-8', function(err, data) {
 
 
 app.use(async(ctx, next) => {
-
-    await next();
-    if (ctx.path == '/') {
-        await fs.readFile('./index.html', 'utf-8', function(err, data) {
+    fs.readFile('./index.html', 'utf-8', function(err, data) {
+        if (err) { console.log(err) } else {
             console.log(data);
-            ctx.body = data;
-            ctx.type = 'text/html';
-        })
-    } else {}
-    await fs.readFile(path, 'utf-8', function(err, data) {
-        console.log(data);
-        ctx.body = data;
-        if (ctx.request.path.slice(-3) === 'css') {
-            ctx.type = 'text/css';
-        } else if (ctx.request.path.slice(-3) === '.js') {
-            ctx.type = 'application/javascript';
+            ctx.response.type = 'text/html';
+            ctx.response.body = data;
+
         }
     })
 
